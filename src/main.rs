3152d01e1eebcf86da32d4de8849c18089b9439a
Benjamin Weber <mail@bwe.im>
2020-04-30 21:16:43 +0200

    it compiles!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
 extern crate tiberius;
 extern crate tokio;
-use tokio::runtime::current_thread;
-use futures::future::{FutureResult};
-use futures::Future;
+use futures::future::FutureResult;
 use futures::stream::Stream;
+use futures::Future;
 use tiberius::SqlConnection;
+use tokio::runtime::current_thread;
 
 // https://github.com/steffengy/tiberius/issues/49#issuecomment-377310318
 // https://github.com/steffengy/tiberius/issues/51#issuecomment-380084937
 
-fn folder(t: tiberius::Transaction<std::boxed::Box<dyn tiberius::BoxableIo>>) {
-    futures::stream::iter_ok::<_, ()>(vec![1, 2, 3])
-        .fold(t, |tx, v| tx.exec("SQL QUERY", &[&v])
-        .and_then(|(result, tx)| Ok((result, tx)) ))
+fn folder(
+    t: tiberius::Transaction<std::boxed::Box<dyn tiberius::BoxableIo>>,
+) -> impl Future<
+    Item = tiberius::Transaction<std::boxed::Box<dyn tiberius::BoxableIo>>,
+    Error = tiberius::Error,
+> {
+    futures::stream::iter_ok(vec![1, 2, 3]).fold(t, |tx, v| {
+        tx.exec("SQL QUERY", &[&v]).and_then(|(_, tx)| Ok(tx))
+    })
 }
 
 fn exec_query(conn_str: &str) {
     let future = SqlConnection::connect(conn_str)
-    .and_then(|conn| conn.transaction())
-    .and_then(|t| folder(t))
-    .and_then(|trans| trans.commit());
+        .and_then(|conn| conn.transaction())
+        .and_then(|t| folder(t))
+        .and_then(|trans| trans.commit())
+        .map_err(|err| panic!(err));
     current_thread::block_on_all(future).unwrap();
 }
 
